9a6439f4d457a82330e9a60b5075e21ff11c23ed
Gilbert Andrei Floarea <gilbertandrei@gmail.com>
2019-04-30 11:33:15 +0300

    Initial Commit

---
+++ HotelAuditWithFRC-coreData5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelAuditWithFRC-coreData5
+//
+//  Created by Gilbert Andrei Floarea on 30/04/2019.
+//  Copyright © 2019 Gilbert Andrei Floarea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
